f3a54cabbf521c49b78ca3f7e285aa87774c5e4c
kongdev <nopaket_kong@hotmail.com>
2025-09-17 00:12:43 +0700

    modify bash docker build

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,9 @@
 
 echo "Building Backstage Docker containers..."
 
+
+
+
 # Install dependencies and build
 echo "Installing dependencies..."
 yarn install --immutable
@@ -19,6 +22,13 @@ yarn workspace app build
 echo "Building and starting containers..."
 docker compose up --build
 
+
+# docker compose stop backend && docker compose run --rm backend node packages/backend migrate --config app-config.yaml --config app-config.docker.yaml
+# docker compose exec postgres psql -U backstage -d backstage -c "\dt"
+
 echo "Backstage is running at:"
 echo "Frontend: http://localhost:3000"
 echo "Backend: http://localhost:7007"
+
+
+docker compose run --rm backend node packages/backend migrate --config app-config.yaml --config app-config.docker.yaml
